91b7c7d017e063c820276d8fc62adc48de579917
Daria Raspitina <68968082+Daria-Ras@users.noreply.github.com>
2020-08-04 19:26:15 +0300

    Update Friends_cities.py

--- Friends_cities.py
+++ Friends_cities.py
@@ -3,8 +3,6 @@ friends_names = ['Аня', 'Коля', 'Лёша', 'Лена', 'Миша']
 friends_cities = ['Владивосток', 'Красноярск', 'Москва', 'Обнинск', 'Чебоксары']
 
 friends = {}
-
-# допишите ваш код сюда
 for i in range(0, len(friends_names)):
     friends[friends_names[i]] = friends_cities[i]
 print('Лена живет в городе ' + friends['Лена'])
